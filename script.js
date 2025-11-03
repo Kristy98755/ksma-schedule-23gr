2655612f4f11401b2faab80abeb3c59e82bb8cb4
Kristy98755 <121288707+Kristy98755@users.noreply.github.com>
2025-11-03 07:22:39 +0600

    Update script.js

--- script.js
+++ script.js
@@ -35,9 +35,9 @@ document.addEventListener("DOMContentLoaded", function() {
         // TARGETED
 
         // CURRENT WEEK
-        "Клиническая биохимия|Практика|NextWeek": "<a href='biohimia.html'>Клиническая биохимия</a>",
-        "Общая гигиена|Практика|NextWeek": "<a href='https://jumpshare.com/share/GTXxYzRwRTJXCXdYdSCO'>Общая гигиена</a>",
-		"Пропедевтика детских болезней|Практика|CurrWeek":"<a href='https://jumpshare.com/share/2XQAo3Z7vOH7BqQ2oZGL'>Пропедевтика детских болезней</a>",
+        "Клиническая биохимия|Практика|CurrWeek": "<a href='biohimia.html'>Клиническая биохимия</a>",
+        "Общая гигиена|Практика|CurrWeek": "<a href='https://jumpshare.com/share/GTXxYzRwRTJXCXdYdSCO'>Общая гигиена</a>",
+		//"Пропедевтика детских болезней|Практика|CurrWeek":"<a href='https://jumpshare.com/share/2XQAo3Z7vOH7BqQ2oZGL'>Пропедевтика детских болезней</a>",
         // "Топографическая анатомия|Практика|CurrWeek": "<a href='https://chatgpt.com/share/68da1b47-05b8-800b-a282-c820fbc39c90'>Топографическая анатомия</a>",
         // "Пропедевтика хирургических болезней|Практика|CurrWeek": "<a href='https://jumpshare.com/share/syI8ek5svsVR2PXNsERj'>Пропедхирургия</a>",
 		// "Лучевая диагностика|Практика|CurrWeek":"<a href='https://chatgpt.com/s/t_68de359569408191b94740fafc98bcbb'>Лучевая диагностика</a>",
@@ -46,9 +46,9 @@ document.addEventListener("DOMContentLoaded", function() {
 		// "Патофизиология|Практика|CurrWeek":"<a href='https://jumpshare.com/share/bFouq17EaDhboPQnckgz'>Патфиз</a>",
 		// "Клиническая биохимия|Практика|NextWeek": "<a href='https://jumpshare.com/share/Vtj3G9a2IRZP3lIKZ0sB'>Клиническая биохимия</a>",
         // "Общая гигиена|Практика|NextWeek": "<a href='https://jumpshare.com/share/gdZygLaXlUUUKpbJ1pzm'>Общая гигиена</a>",
-		"Топографическая анатомия|Практика|CurrWeek": "<a href='topan.html'>Топографическая анатомия</a><br><span style='font-size: 12px;'>Повторите прошлую тему!</span>",
+		//"Топографическая анатомия|Практика|CurrWeek": "<a href='topan.html'>Топографическая анатомия</a><br><span style='font-size: 12px;'>Повторите прошлую тему!</span>",
 		// "Пропедевтика хирургических болезней|Практика|CurrWeek": "<a href='https://jumpshare.com/share/jxMVck4Xdbi0XTRvhkWV'>Пропедхирургия</a>",
-		"Базисная фармакология|Практика|CurrWeek": "<a href='https://jumpshare.com/share/q0mUpyh1NGYBn2YzP9Zz'>Базисная фармакология</a>",
+		//"Базисная фармакология|Практика|CurrWeek": "<a href='https://jumpshare.com/share/q0mUpyh1NGYBn2YzP9Zz'>Базисная фармакология</a>",
 		// "Лучевая диагностика|Практика|CurrWeek":"<a href='luch.html'>Лучевая диагностика</a>",
 		// "Патологическая анатомия|Практика|NextWeek":"<a href='https://jumpshare.com/share/zrUZiGuQWqKXtCVVJYpM'>Патологическая анатомия</a>"
         // NEXT WEEK
