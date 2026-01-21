877b828595866ef2e84ddb0b8565f00b29a37d64
Kristy98755 <I.g.o.r.56@mail.ru>
2026-01-21 16:28:01 +0600

    uncomment luch

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ checkUpdate();
 		// "Топографическая анатомия|Практика|CurrWeek": "<a href='https://jumpshare.com/share/pStjOoBpXgybf99pre2l'>Топографическая анатомия</a>",
 		// "Пропедевтика хирургических болезней|Практика|CurrWeek": "<a href='https://jumpshare.com/share/jxMVck4Xdbi0XTRvhkWV'>Пропедхирургия</a>",
 		// "Базисная фармакология|Практика|CurrWeek": "<a href='https://jumpshare.com/share/q0mUpyh1NGYBn2YzP9Zz'>Базисная фармакология</a>",
-		// "Лучевая диагностика и терапия|Практика|CurrWeek":"<a href='https://kristy98755.github.io/debate'>Лучевая диагностика</a>",
+		"Лучевая диагностика и терапия|Практика|CurrWeek":"<a href='https://kristy98755.github.io/debate'>Лучевая диагностика</a>",
    //"НИРС|Практика|CurrWeek": "<a href='https://jumpshare.com/share/GbUW87yUWiPTBeQzL2z3'>НИРС</a>",
 			// "Урология|Практика|CurrWeek":"<a href='urology.html'>Урология</a>",
 			// "Оториноларингология|Практика|CurrWeek":"<a href='lor.html'>Оториноларингология</a>",
