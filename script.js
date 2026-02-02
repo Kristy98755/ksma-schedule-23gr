8e7c4a5a6bdb869eb53d189e9aaf9ea3ff00c7a2
Kristy98755 <I.g.o.r.56@mail.ru>
2026-02-02 22:39:36 +0600

    MIKUDAYOOOOO!

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 }
 
-// checkUpdate();
+checkUpdate();
 		
 /* ============================================================
    Schedule Patcher Engine
