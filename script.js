567ac4779a296ca89ab0d37247f22bd57e3a1893
Kristy98755 <I.g.o.r.56@mail.ru>
2026-02-20 19:23:27 +0600

    Update script.js

--- script.js
+++ script.js
@@ -294,6 +294,7 @@ function removeEmptyDays() {
 		// "ВМП-ОТМС|Практика|CurrWeek":"<a href='https://jumpshare.com/share/W378sP6WnSnSTv5mmMUr'>ВМП-ОТМС</a>",
 		"Пропедхирургия|Практика|CurrWeek":"<a href='propedhir.html'>Пропедхирургия</a>",
 		"Госпитальная терапия|Практика|CurrWeek":"<a href='https://jumpshare.com/share/tQo6ald1BhrYItmHZJyW'>Госпитальная терапия</a>",
+		"Неврология|Практика|CurrWeek":"<a href='https://jumpshare.com/share/q7ZsnqWSqCnQgL1OCCxs'>Неврология</a>",
 
         // NEXT WEEK
 		// "Акушерство и гинекология|Практика|CurrWeek": "<a href='ginecology.html'>Акушерство и гинекология</a>",
